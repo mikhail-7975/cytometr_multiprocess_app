c4c081245241236303aa51d497ae073975b55857
mikhail-7975 <mikhail.kalinichenko.1999@gmail.com>
2021-02-12 17:40:44 +0700

    code for server and client. cant connect

--- client/client/main.cpp
+++ client/client/main.cpp
@@ -1,5 +1,84 @@
-#include <iostream>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <stdio.h>
+
+#define DEFAULT_PORT "27015"
+
+#pragma comment(lib, "Ws2_32.lib")
 
 int main() {
-	std::cout << "hello world!" << std::endl;
+	WSADATA wsaData;
+	int iResult;
+
+	// Initialize Winsock
+	iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
+	if (iResult != 0) {
+		printf("WSAStartup failed: %d\n", iResult);
+		return 1;
+	} else {
+		printf("WSAStartup not failed: %d\n", iResult);
+	}
+
+//================================================
+	struct addrinfo* result = NULL,
+		* ptr = NULL,
+		hints;
+
+	ZeroMemory(&hints, sizeof(hints));
+	hints.ai_family = AF_INET;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_protocol = IPPROTO_TCP;
+//===============================================
+// Resolve the server address and port
+	char addr[] = "0.0.0.0";
+	iResult = getaddrinfo(/*argv[1]*/addr, DEFAULT_PORT, &hints, &result);
+	if (iResult != 0) {
+		printf("getaddrinfo failed: %d\n", iResult);
+		WSACleanup();
+		return 1;
+	}
+	else {
+		printf("getaddrinfo ok: %d\n", iResult);
+	}
+//===============================================
+	SOCKET ConnectSocket = INVALID_SOCKET;
+//===============================================
+	// Attempt to connect to the first address returned by the call to getaddrinfo
+	ptr = result;
+
+	// Create a SOCKET for connecting to server
+	ConnectSocket = socket(ptr->ai_family, ptr->ai_socktype, ptr->ai_protocol);
+	if (ConnectSocket == INVALID_SOCKET) {
+		printf("Error at socket(): %ld\n", WSAGetLastError());
+		freeaddrinfo(result);
+		WSACleanup();
+		return 1;
+	}
+	else {
+		printf("No errors\n");
+	}
+//===============================================
+	// Connect to server.
+	iResult = connect(ConnectSocket, ptr->ai_addr, (int)ptr->ai_addrlen);
+	if (iResult == SOCKET_ERROR) {
+		closesocket(ConnectSocket);
+		ConnectSocket = INVALID_SOCKET;
+	}
+
+	// Should really try the next address returned by getaddrinfo
+	// if the connect call failed
+	// But for this simple example we just free the resources
+	// returned by getaddrinfo and print an error message
+
+	freeaddrinfo(result);
+
+	if (ConnectSocket == INVALID_SOCKET) {
+		printf("Unable to connect to server!\n");
+		WSACleanup();
+		return 1;
+	} else {
+		printf("Connected!\n");
+	}
+
+	return 0;
 }
--- server/main.py
+++ server/main.py
@@ -1,1 +1,18 @@
-print("hello world!")
+import socket
+
+sock = socket.socket()
+sock.bind(('', 27015))
+print(sock.getsockname())
+print("listen...")
+sock.listen(100)
+conn, addr = sock.accept()
+
+print('connected:', addr)
+
+while True:
+    data = conn.recv(27015)
+    if not data:
+        break
+    conn.send(data.upper())
+
+conn.close()
