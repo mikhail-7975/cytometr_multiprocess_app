ca1fafc851c490caed33673b894033475a18612b
mikhail-7975 <mikhail.kalinichenko.1999@gmail.com>
2021-02-13 12:07:49 +0700

    class tcpClient on c++

--- client/client/main.cpp
+++ client/client/main.cpp
@@ -2,126 +2,130 @@
 #include <ws2tcpip.h>
 #include <stdio.h>
 
-#define DEFAULT_PORT "9090"
 #define DEFAULT_BUFLEN 512
 #pragma comment(lib, "Ws2_32.lib")
 
-int main() {
+class tcpClient {
 	WSADATA wsaData;
 	int iResult;
-
-	// Initialize Winsock
-	iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
-	if (iResult != 0) {
-		printf("WSAStartup failed: %d\n", iResult);
-		return 1;
-	} else {
-		printf("WSAStartup not failed: %d\n", iResult);
-	}
-
-//================================================
-	struct addrinfo* result = NULL,
-		* ptr = NULL,
-		hints;
-
-	ZeroMemory(&hints, sizeof(hints));
-	hints.ai_family = AF_INET;
-	hints.ai_socktype = SOCK_STREAM;
-	hints.ai_protocol = IPPROTO_TCP;
-//===============================================
-// Resolve the server address and port
-	char addr[] = "127.0.0.1";
-	iResult = getaddrinfo(/*argv[1]*/addr, DEFAULT_PORT, &hints, &result);
-	if (iResult != 0) {
-		printf("getaddrinfo failed: %d\n", iResult);
-		WSACleanup();
-		return 1;
-	}
-	else {
-		printf("getaddrinfo ok: %d\n", iResult);
-	}
-//===============================================
-	SOCKET ConnectSocket = INVALID_SOCKET;
-//===============================================
-	// Attempt to connect to the first address returned by the call to getaddrinfo
-	ptr = result;
-
-	// Create a SOCKET for connecting to server
-	ConnectSocket = socket(ptr->ai_family, ptr->ai_socktype, ptr->ai_protocol);
-	if (ConnectSocket == INVALID_SOCKET) {
-		printf("Error at socket(): %ld\n", WSAGetLastError());
-		freeaddrinfo(result);
-		WSACleanup();
-		return 1;
-	}
-	else {
-		printf("No errors\n");
-	}
-//===============================================
-	// Connect to server.
-	iResult = connect(ConnectSocket, ptr->ai_addr, (int)ptr->ai_addrlen);
-	if (iResult == SOCKET_ERROR) {
-		printf("Socket connect error: %d\n", WSAGetLastError());
-		closesocket(ConnectSocket);
-		ConnectSocket = INVALID_SOCKET;
-	}
-
-	// Should really try the next address returned by getaddrinfo
-	// if the connect call failed
-	// But for this simple example we just free the resources
-	// returned by getaddrinfo and print an error message
-
-	freeaddrinfo(result);
-
-	if (ConnectSocket == INVALID_SOCKET) {
-		printf("Unable to connect to server!\n");
-		WSACleanup();
-		return 1;
-	} else {
-		printf("Connected!\n");
-	}
-//=======================================================
-	int recvbuflen = DEFAULT_BUFLEN;
+	struct addrinfo *result = NULL, *ptr = NULL, hints;
+	char addr[10] = "127.0.0.1";
+	char DEFAULT_PORT[5] = "9090";
+	SOCKET ConnectSocket;
 
 	const char* sendbuf = "this is a test";
+	int recvbuflen = DEFAULT_BUFLEN;
 	char recvbuf[DEFAULT_BUFLEN];
 
-	//int iResult;
+public:
+	tcpClient() {
+		iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
+		if (iResult != 0) {
+			printf("WSAStartup failed: %d\n", iResult);
+		}
+		else {
+			printf("WSAStartup not failed: %d\n", iResult);
+		}
 
-	// Send an initial buffer
-	iResult = send(ConnectSocket, sendbuf, (int)strlen(sendbuf), 0);
-	if (iResult == SOCKET_ERROR) {
-		printf("send failed: %d\n", WSAGetLastError());
-		closesocket(ConnectSocket);
-		WSACleanup();
-		return 1;
+		ZeroMemory(&hints, sizeof(hints));
+		hints.ai_family = AF_INET;
+		hints.ai_socktype = SOCK_STREAM;
+		hints.ai_protocol = IPPROTO_TCP;
+
+		iResult = getaddrinfo(/*argv[1]*/addr, DEFAULT_PORT, &hints, &result);
+		if (iResult != 0) {
+			printf("getaddrinfo failed: %d\n", iResult);
+			WSACleanup();
+		} else {
+			printf("getaddrinfo ok: %d\n", iResult);
+		}
+
+		ConnectSocket = INVALID_SOCKET;
+		
+		ptr = result;
+
+		ConnectSocket = socket(ptr->ai_family, ptr->ai_socktype, ptr->ai_protocol);
+		if (ConnectSocket == INVALID_SOCKET) {
+			printf("Error at socket(): %ld\n", WSAGetLastError());
+			freeaddrinfo(result);
+			WSACleanup();
+		} else {
+			printf("No errors\n");
+		}
+
+		iResult = connect(ConnectSocket, ptr->ai_addr, (int)ptr->ai_addrlen);
+		if (iResult == SOCKET_ERROR) {
+			printf("Socket connect error: %d\n", WSAGetLastError());
+			closesocket(ConnectSocket);
+			ConnectSocket = INVALID_SOCKET;
+		}
+
+		freeaddrinfo(result);
+
+		if (ConnectSocket == INVALID_SOCKET) {
+			printf("Unable to connect to server!\n");
+			WSACleanup();
+		} else {
+			printf("Connected!\n");
+		}
+
+
+	}
+	int tcpSend() {
+		iResult = send(ConnectSocket, sendbuf, (int)strlen(sendbuf), 0);
+		if (iResult == SOCKET_ERROR) {
+			printf("send failed: %d\n", WSAGetLastError());
+			//closesocket(ConnectSocket);
+			//WSACleanup();
+			return iResult;
+		} else{
+			printf("Sent ok\n");
+			printf("Bytes Sent: %ld\n", iResult);
+		}
+		return iResult;
 	}
 
-	printf("Bytes Sent: %ld\n", iResult);
-
-	// shutdown the connection for sending since no more data will be sent
-	// the client can still use the ConnectSocket for receiving data
-	iResult = shutdown(ConnectSocket, SD_SEND);
-	if (iResult == SOCKET_ERROR) {
-		printf("shutdown failed: %d\n", WSAGetLastError());
-		closesocket(ConnectSocket);
-		WSACleanup();
-		return 1;
+	int tcpReceive() {
+		//do {
+			iResult = recv(ConnectSocket, recvbuf, recvbuflen, 0);
+			printf("iResult == %d\n", iResult);
+			if (iResult > 0) {
+				printf("Bytes received: %d\n", iResult);
+			} else {
+				if (iResult == 0) {
+					printf("Connection closed\n");
+				}
+				else {
+					printf("recv failed: %d\n", WSAGetLastError());
+					iResult = -1;
+				}
+			}
+		//} while (iResult > 0);
+		return iResult;
 	}
 
-	// Receive data until the server closes the connection
-	do {
-		iResult = recv(ConnectSocket, recvbuf, recvbuflen, 0);
-		if (iResult > 0)
-			printf("Bytes received: %d\n", iResult);
-		else if (iResult == 0)
-			printf("Connection closed\n");
-		else
-			printf("recv failed: %d\n", WSAGetLastError());
-	} while (iResult > 0);
+	~tcpClient() {
+		closesocket(ConnectSocket);
+		WSACleanup();
+	}
+};
+
+int main() {
+	tcpClient client = tcpClient();
+	for(int i = 0; i < 1000; i++) {
+		printf("%d: \n", i);
+		client.tcpSend();
+		client.tcpReceive();
+		Sleep(2000);
+	}
+
 	
-	closesocket(ConnectSocket);
-	WSACleanup();
+	/*while (1) {
+		int res = client.tcpReceive();
+		if (res == -1)
+			break;
+		printf(".");
+		Sleep(0.5);
+	}*/
 	return 0;
-
 }
--- server/main.py
+++ server/main.py
@@ -10,11 +10,14 @@ conn, addr = sock.accept()
 print('connected:', addr)
 
 while True:
-    data = conn.recv(9090)
-    if not data:
+    cmd = input()
+    if cmd == "rcv":
+        data = conn.recv(9090)
+        print(data)
+    if cmd == "snd":
+        conn.send(b'test responce')
+    if cmd == "cls":
+        conn.close()
         break
-        #print(data)
-    print(data)
-    conn.send(data.upper())
 
-conn.close()
+print("connection closed")
