a25a1da80f7559922a78bdfe0851f84621c37334
mikhail-7975 <mikhail.kalinichenko.1999@gmail.com>
2021-02-16 12:57:17 +0700

    created condition for completing preprocessor thread

--- client/client/data_preprocessing.cpp
+++ client/client/data_preprocessing.cpp
@@ -12,13 +12,16 @@ static std::mutex rawDataQMtx;
 
 void data_preprocessor(std::list<std::vector<uint16_t>> &rawDataQueue, std::list<std::vector<int>>& tracesQueue)
 {
-	for (int i = 0; ; i++) {
-		
+	auto start_time = std::chrono::system_clock::now();
+	//auto end_time = std::chrono::system_clock::now();
+	auto now = std::chrono::system_clock::now();
+	for (int i = 0; (std::chrono::system_clock::now() - start_time).count() < 100000; i++) {
 		if (rawDataQueue.size() != 0) {
 			rawDataQMtx.lock();
-			std::cout <<"size = " << rawDataQueue.size() << "; pop from queue" << std::endl;
+			std::cout << "size = " << rawDataQueue.size() << "; pop from queue\n";
 			rawDataQueue.pop_back();
 			rawDataQMtx.unlock();
+			start_time = std::chrono::system_clock::now();
 		}
 	}
 }
--- client/client/data_reading.cpp
+++ client/client/data_reading.cpp
@@ -14,7 +14,7 @@ static std::mutex rawDataQMtx;
 
 void data_reader(std::list<std::vector<uint16_t>> &rawDataQueue)
 {
-	for (int i = 0; i < 100; i++) {
+	for (int i = 0; i < 10000; i++) {
 		uint16_t baseine_input[TRACE_LEN];
 		for (int i = 0; i < TRACE_LEN; i++) {
 			baseine_input[i] = i + 1;
@@ -23,11 +23,10 @@ void data_reader(std::list<std::vector<uint16_t>> &rawDataQueue)
 		memcpy(inpStr, baseine_input, TRACE_LEN * TRACE_ELEM_SIZE);
 		uint16_t data[TRACE_LEN];
 		memcpy(data, inpStr, TRACE_LEN * TRACE_ELEM_SIZE);
-		data;
 		rawDataQMtx.lock();
 		rawDataQueue.push_back(std::vector<uint16_t>(data, data + TRACE_LEN));
-		std::cout << "pushed to queue" << std::endl;
+		std::cout << "pushed to queue\n";// << std::endl;
 		rawDataQMtx.unlock();
-		std::this_thread::sleep_for(std::chrono::milliseconds(5));
+		//std::this_thread::sleep_for(std::chrono::milliseconds(5));
 	}
 }
